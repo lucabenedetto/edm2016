fae7996f9763693342f8c933d9ffbf971acbe1b5
Luca Benedetto <luca.benedetto93@gmail.com>
2019-02-20 15:33:49 +0100

    renamed script

--- rnn_prof/data/script_reinstall.sh
+++ rnn_prof/data/script_reinstall.sh
@@ -1,4 +1,4 @@
 cd ../..
 python setup.py install
 cd rnn_prof/data
-rnn_prof irt assistments skill_builder_data_corrected_big.txt --onepo --drop-duplicates --no-remove-skill-nans --num-folds 5 --item-id-col problem_id --concept-id-col single
+# rnn_prof irt assistments skill_builder_data_corrected_big.txt --onepo --drop-duplicates --no-remove-skill-nans --num-folds 5 --item-id-col problem_id --concept-id-col single
