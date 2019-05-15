c6f9a60bf713dcc522a95ab582e346184336ecaa
Luca Benedetto <luca.benedetto93@gmail.com>
2019-05-15 09:47:37 +0200

    fixed typo

--- rnn_prof/data/create_by_skill_dataset_kdd.py
+++ rnn_prof/data/create_by_skill_dataset_kdd.py
@@ -7,6 +7,6 @@ def create_dataset_by_skill(skill_id):
     df.to_csv('by_skill_kdd/bridge_to_algebra_2006_2007_train_' + str(skill_id) + '.csv')
 
 df = pd.read_csv('bridge_to_algebra_2006_2007_train.txt', sep='\t', low_memory=False)
-skill_list = df.skill_id.unique()
+skill_list = df['KC(SubSkills)'].unique()
 for skill in skill_list:
     create_dataset_by_skill(skill)
