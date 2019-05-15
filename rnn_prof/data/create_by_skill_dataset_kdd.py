e4c1d4e6221a38e4ad46da554f9d3380ac7e5bdd
Luca Benedetto <luca.benedetto93@gmail.com>
2019-05-15 11:56:03 +0200

    updated script for creating kdd datasets filtered by skill

--- rnn_prof/data/create_by_skill_dataset_kdd.py
+++ rnn_prof/data/create_by_skill_dataset_kdd.py
@@ -1,12 +1,40 @@
 import pandas as pd
 
-def create_dataset_by_skill(skill_id):
-    df = pd.read_csv('bridge_to_algebra_2006_2007_train.txt', sep='\t', low_memory=False)
+def get_item_skill_mapping(filepath):
+    df = pd.read_csv(filepath, sep='\t', low_memory=False)
     df[['KC(SubSkills)']] = df[['KC(SubSkills)']].fillna(value='.')
-    df = df[df['KC(SubSkills)']==skill_id]
-    df.to_csv('by_skill_kdd/bridge_to_algebra_2006_2007_train_' + str(skill_id) + '.csv')
+    df['question'] = df.apply(lambda r: r['Problem Name'] + '_' + r['Step Name'], axis=1)
+    df = df.drop_duplicates('question')
+    skill_set = set()
+    for skills in df['KC(SubSkills)'].values:
+        for skill in skills.split(' -- '):
+            skill_set.add(skill)
+    skill_dict = {name: idx for (idx, name) in enumerate(skill_set)}
+    result_df = pd.DataFrame(columns=['question', 'skill_id', 'skill_name'])
+    for question, skills in df[['question', 'KC(SubSkills)']].values:
+        for skill in skills.split(' -- '):
+            result_df = result_df.append(
+                {'question': question, 'skill_id': skill_dict.get(skill), 'skill_name': skill}, ignore_index=True)
+    return result_df
 
-df = pd.read_csv('bridge_to_algebra_2006_2007_train.txt', sep='\t', low_memory=False)
-skill_list = df['KC(SubSkills)'].unique()
-for skill in skill_list:
-    create_dataset_by_skill(skill)
+#####
+
+filepath = 'bridge_to_algebra_2006_2007_train.txt'
+
+df = pd.read_csv(filepath, sep='\t', low_memory=False)
+df['question'] = df.apply(lambda r: r['Problem Name'] + '_' + r['Step Name'], axis=1)
+print('[INFO] Collected dataframe')
+
+item_skill_mapping_df = get_item_skill_mapping(filepath)
+print('[INFO] Genereted item skill mapping')
+
+skill_list = item_skill_mapping_df['skill_id'].unique()
+print('[INFO] Created list of skills')
+
+for skill_id in skill_list[:2]:
+    print('[INFO] Working on skill', skill_id)
+    list_items_to_keep = list(item_skill_mapping_df[item_skill_mapping_df['skill_id']==skill_id]['question'].values)
+    df = df[df['question'].isin()]
+    df.drop('question').to_csv('by_skill_kdd/bridge_to_algebra_2006_2007_train_' + str(skill_id) + '.csv')
+    print('[INFO] Stored DF for the skill')
+
