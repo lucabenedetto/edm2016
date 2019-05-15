2eb1da3433fd59aefa7ff70dd8a8b902e401c84e
Luca Benedetto <luca.benedetto93@gmail.com>
2019-05-15 09:46:49 +0200

    created script for creating dataset by skill

---
+++ rnn_prof/data/create_by_skill_dataset_kdd.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def create_dataset_by_skill(skill_id):
+    df = pd.read_csv('bridge_to_algebra_2006_2007_train.txt', sep='\t', low_memory=False)
+    df[['KC(SubSkills)']] = df[['KC(SubSkills)']].fillna(value='.')
+    df = df[df['KC(SubSkills)']==skill_id]
+    df.to_csv('by_skill_kdd/bridge_to_algebra_2006_2007_train_' + str(skill_id) + '.csv')
+
+df = pd.read_csv('bridge_to_algebra_2006_2007_train.txt', sep='\t', low_memory=False)
+skill_list = df.skill_id.unique()
+for skill in skill_list:
+    create_dataset_by_skill(skill)
