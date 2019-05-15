cf7f92da23e1d891c5eb17e2f9977614cbb37037
Luca Benedetto <luca.benedetto93@gmail.com>
2019-05-15 09:40:16 +0200

    renamed method working on assistments only

--- rnn_prof/data/create_skill_dataset.py
+++ rnn_prof/data/create_skill_dataset.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-def create_dataset_by_skill(skill_id):
+def create_dataset_by_skill_assistments(skill_id):
     df = pd.read_csv('skill_builder_data_corrected.csv', low_memory=False)
     df = df[df['skill_id']==skill_id]
-    df.to_csv('by_skill/skill_builder_data_corrected_' + str(skill_id) + '.csv')
+    df.to_csv('by_skill_assistments/skill_builder_data_corrected_' + str(skill_id) + '.csv')
 
 df = pd.read_csv('skill_builder_data_corrected.csv', low_memory=False)
 skill_list = df.skill_id.unique()
 for skill in skill_list:
-    create_dataset_by_skill(skill)
+    create_dataset_by_skill_assistments(skill)
