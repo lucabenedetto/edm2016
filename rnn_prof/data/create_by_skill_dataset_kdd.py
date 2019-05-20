d9597a7fe4f37511cbc177d81fc391b8e0571cb3
Luca Benedetto <luca.benedetto93@gmail.com>
2019-05-20 17:28:06 +0200

    fixed script for creating kdd split by skill

--- rnn_prof/data/create_by_skill_dataset_kdd.py
+++ rnn_prof/data/create_by_skill_dataset_kdd.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 def get_item_skill_mapping(filepath):
     df = pd.read_csv(filepath, sep='\t', low_memory=False)
     df[['KC(SubSkills)']] = df[['KC(SubSkills)']].fillna(value='.')
@@ -17,7 +18,6 @@ def get_item_skill_mapping(filepath):
                 {'question': question, 'skill_id': skill_dict.get(skill), 'skill_name': skill}, ignore_index=True)
     return result_df
 
-#####
 
 filepath = 'bridge_to_algebra_2006_2007_train.txt'
 
@@ -27,16 +27,17 @@ df['question'] = df.apply(lambda r: r['Problem Name'] + '_' + r['Step Name'], ax
 print('[INFO] Collected dataframe')
 
 item_skill_mapping_df = get_item_skill_mapping(filepath)
-print('[INFO] Genereted item skill mapping')
+print('[INFO] Generated item skill mapping')
 
-skill_list = item_skill_mapping_df['skill_id'].unique()
-print('[INFO] Created list of skills')
+df_mapping_list = pd.DataFrame(columns=['skill_id', 'skill_name'])
 
-for skill_id in skill_list:
-    print('[INFO] Working on skill %d' %skill_id)
-    list_items_to_keep = list(item_skill_mapping_df[item_skill_mapping_df['skill_id']==skill_id]['question'].values)
+for skill_id, skill_name in item_skill_mapping_df[['skill_id', 'skill_name']].values:
+    print('[INFO] Working on skill %d' % skill_id)
+    list_items_to_keep = list(item_skill_mapping_df[item_skill_mapping_df['skill_id'] == skill_id]['question'].values)
     local_df = df[df['question'].isin(list_items_to_keep)]
-    print('[INFO] Number of rows %d' %len(local_df.index))
-    local_df[original_columns].to_csv('by_skill_kdd/bridge_to_algebra_2006_2007_train_%d.txt' %skill_id, sep='\t')
+    print('[INFO] Number of rows %d' % len(local_df.index))
+    local_df[original_columns].to_csv('by_skill_kdd_NEW/bridge_to_algebra_2006_2007_train_%d.txt' % skill_id, sep='\t')
     print('[INFO] Stored DF for the skill')
+    df_mapping_list.append({'skill_id': skill_id, 'skill_name': skill_name}, ignore_index=True)
 
+df_mapping_list.to_csv('by_skill_kdd_mapping_idx2name.csv', sep='\t')
